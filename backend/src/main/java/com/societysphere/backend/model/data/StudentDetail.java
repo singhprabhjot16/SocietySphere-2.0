9981593660668df8e7c901234b52f862a0138b93
Prabhjot Singh <prabhjots5498@gmail.com>
2025-03-09 14:15:44 +0530

    Table Creation and Entity Class

---
+++ backend/src/main/java/com/societysphere/backend/model/data/StudentDetail.java
@@ -0,0 +1,38 @@
+package com.societysphere.backend.model.data;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "STUDENT_DETAIL")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StudentDetail implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "STATUS")
+    private String status;
+
+}
